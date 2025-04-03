122aecd7e0c17ebfcd6316bd53cd41fb8dee000a
NedBen <39988307+xTbeniaminTx@users.noreply.github.com>
2025-04-03 01:21:11 +0000

    intl installed for internationalisation time in french

--- admin/table1_new_form.php
+++ admin/table1_new_form.php
@@ -10,30 +10,36 @@
 <p>ajouter ici un album</p>
 <hr>
 <form method="POST" action="table1_new_valide.php" enctype="multipart/form-data">
-    nom:<input type="text" name="nom"><br>
-    age:<input type="number" name="age"><br>
-    poids:<input type="float" name="poids"><br>
-    race:<input type="text" name="race"><br>
-    genre:<input type="number" name="genre"><br>
-    Photo : <input type="file" name="photo" required /><br />
+    nom: <input type="text" name="nom"><br>
+    age: <input type="number" name="age"><br>
+    poids: <input type="text" name="poids"><br>
+    race: <input type="text" name="race"><br>
+    
+    <!-- Radio buttons for genre -->
+    genre:<br>
+    <label>
+        <input type="radio" name="genre" value="1" checked> Male
+    </label><br>
+    <label>
+        <input type="radio" name="genre" value="0"> Female
+    </label><br>
+    
+    Photo: <input type="file" name="photo" required /><br />
+    
     propriétaire:
-        <select name="proprio">
-                <?php
-                $mabd = new PDO('mysql:host=db;dbname=sae203Base;charset=UTF8;', 'sae203User', 'MonSuperMotDePasse1');
-                $mabd->query('SET NAMES utf8;');
-                $req = "SELECT proprio_nom, proprio_prenom, proprio_id FROM proprietaires ";
-                $resultat = $mabd->query($req);
-
-        foreach ($resultat as $value) {
-            echo '<option value='.$value["proprio_id"].'>'. $value["proprio_nom"] . ' ' . $value["proprio_prenom"] . '</option>';
-        }
+    <select name="proprio">
+        <?php
+            $mabd = new PDO('mysql:host=db;dbname=sae203Base;charset=UTF8;', 'sae203User', 'MonSuperMotDePasse1');
+            $mabd->query('SET NAMES utf8;');
+            $req = "SELECT proprio_nom, proprio_prenom, proprio_id FROM proprietaires";
+            $resultat = $mabd->query($req);
+            foreach ($resultat as $value) {
+                echo '<option value="' . $value["proprio_id"] . '">' . $value["proprio_nom"] . ' ' . $value["proprio_prenom"] . '</option>';
+            }
         ?>
-        </select>
-
+    </select>
+    <br>
     <input type="submit" name="Sauvegarde" value="Sauvegarde">
 </form>
-
-</tbody>
-</table>
 </body>
 </html>
--- listing.php
+++ listing.php
@@ -1,32 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Nos Albums</title>
 </head>
 <body>
-<a href="index.php">Accueil</a> | <a href="catalogue.php"> catalogue </a> | <a href="admin/table1_gestion.php">admin</a>	
-	<hr>
-<h1>nos albums</h1>
+<a href="index.php">Accueil</a> | <a href="catalogue.php">Catalogue</a> | <a href="admin/table1_gestion.php">Admin</a>	
+<hr>
+<h1>Nos Albums</h1>
 <hr>
 <?php
+// Connect to the database
 $mabd = new PDO('mysql:host=db;dbname=sae203Base;charset=UTF8;', 'sae203User', 'MonSuperMotDePasse1');
 $mabd->query('SET NAMES utf8;');
+
+// Retrieve chats joined with proprietaires
 $req = "SELECT * FROM chats INNER JOIN proprietaires ON chats._proprio_id = proprietaires.proprio_id";
 $resultat = $mabd->query($req);
 
 foreach ($resultat as $value) {
-    echo '<div>' ;
-    echo '<h3>'.$value['chat_nom'] . '</h3>';
-    echo '<p>Age: '.$value['chat_age']. ' ans </p>';
-    echo '<p>Race: ' . $value['chat_race'] . ' </p>';
-    echo '<p>' . $value['chat_poids'] . ' kg </p>';
-    echo '<p>Genre: ' . $value['chat_genre'] . ' </p>';
-    echo '<p>Date de naissance: ' . $value['chat_date'] . ' </p>';
-    echo '<p> Propriétaire: ' . $value['proprio_nom'] . '</p>';
-    echo '<img src="images/uploads/' .$value['chat_photo'].'" width="150px">';
+    echo '<div>';
+    echo '<h3>' . $value['chat_nom'] . '</h3>';
+    echo '<p>Age: ' . $value['chat_age'] . ' ans</p>';
+    echo '<p>Race: ' . $value['chat_race'] . '</p>';
+    echo '<p>' . $value['chat_poids'] . ' kg</p>';
+    
+    // Convert numeric genre to text ("Male" if 1, "Female" if 0)
+    $genreText = ($value['chat_genre'] == 1) ? "Male" : "Female";
+    echo '<p>Genre: ' . $genreText . '</p>';
+    
+    // Format the date in French using IntlDateFormatter if a valid date exists
+    $dateValue = $value['chat_date'];
+    if (!empty($dateValue)) {
+        $dateObj = new DateTime($dateValue);
+        $fmt = new IntlDateFormatter(
+            'fr_FR', 
+            IntlDateFormatter::NONE, 
+            IntlDateFormatter::NONE, 
+            'Europe/Paris', 
+            IntlDateFormatter::GREGORIAN
+        );
+        $fmt->setPattern('dd MMMM yyyy');  // e.g. "10 février 2012"
+        $dateFr = $fmt->format($dateObj);
+    } else {
+        $dateFr = "Date non définie";
+    }
+    echo '<p>Date de naissance: ' . $dateFr . '</p>';
+    
+    echo '<p>Propriétaire: ' . $value['proprio_nom'] . '</p>';
+    echo '<img src="images/uploads/' . $value['chat_photo'] . '" width="150px">';
     echo '</div><hr>';
 }
 ?>
-
 </body>
 </html>
